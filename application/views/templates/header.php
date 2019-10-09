475fc8d6b9c96e9c4d755173221be7afd6c76fb5
reza173040113 <reza_173040113@mail.unpas.ac.id>
2019-10-10 01:00:57 +0700

    memodif file header,footer dan home

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,6 +8,27 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title><?php echo $judul;?></title>
   </head>
   <body>
+  	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  		<div class="container">
+	  <a class="navbar-brand" href="#">CI App</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarText">
+	    <ul class="navbar-nav mr-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Mahasiswa</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">About</a>
+	      </li>
+	    </ul>
+	  </div>
+	</div>
+	</nav>
